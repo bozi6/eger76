e39335e38e5ab07e71da6e674a542ff66ba56903
boz <kontab6@gmail.com>
2020-01-16 08:25:55 +0100

    keres.js ben javítgatások

--- public/assets/js/keres.js
+++ public/assets/js/keres.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     if ($("#csoportok").exists()) {
         $("#csoportok").focus();
         $("#csoportok").change(function () { // a legördülő listában változtatunk
-            var csid = $("#csoportok").val();
+            const csid = $("#csoportok").val();
             // A csoportok lekérdezése ajax-al.
             $.ajax({
                 url: "csopval",
@@ -29,28 +29,28 @@ $(document).ready(function () {
                 fellepok = JSON.parse(fellepok);
                 $("#benvan").empty();
                 $("#szamok").empty();
-                var i = 1; // sorszámok megadása
-                var belepett_szama = 0; // beléptetettek száma
-                var lis = "<li class='list-group-item p-1 bg-dark'>";
-                var lisben = "<li class='list-group-item p-1 bg-warning text-dark'>";
-                var chkbox = "; <input class='ml-1' type='checkbox' id='felhaszn' name='fellepo[]' value='";
-                var label = "<label class='form-check-label ml-2' for='felhaszn'>";
+                let i = 1; // sorszámok megadása
+                let belepett_szama = 0; // beléptetettek száma
+                const lis = "<li class='list-group-item p-1 bg-dark'>";
+                const lisben = "<li class='list-group-item p-1 bg-warning text-dark'>";
+                const chkbox = "; <input class='ml-1' type='checkbox' id='felhaszn' name='fellepo[]' value='";
+                const label = "<label class='form-check-label ml-2' for='felhaszn'>";
                 fellepok.forEach(function (fellepo) {
-                    var felhid = fellepo.id;
-                    if (fellepo.belepett == 1) {
-                        $('#benvan').append(lisben + i + chkbox + felhid + '" checked>' + label + fellepo.nev + '</label></li>')
+                    const felhid = fellepo.id;
+                    if (fellepo.belepett === 1) {
+                        $("#benvan").append(lisben + i + chkbox + felhid + "\" checked>" + label + fellepo.nev + "</label></li>");
                         belepett_szama++;
                         i++;
                     } else {
-                        $('#benvan').append(lis + i + chkbox + felhid + '">' + label + fellepo.nev + '</label></li>')
+                        $("#benvan").append(lis + i + chkbox + felhid + "\">" + label + fellepo.nev + "</label></li>");
                         i++;
                     }
                 });
                 i = i - 1;
-                $('#szamok').append("Összesen: " + i + ", ebből: " + belepett_szama + " belépett." + "<br>Szabad jegyek száma: " + (i - belepett_szama));
-            })
-            $('#csopgomb').removeClass('disabled');
-            $('#csopgomb').attr('href', 'csopbel/' + csid);
+                $("#szamok").append("Összesen: " + i + ", ebből: " + belepett_szama + " belépett." + "<br>Szabad jegyek száma: " + (i - belepett_szama));
+            });
+            $("#csopgomb").removeClass("disabled");
+            $("#csopgomb").attr("href", "csopbel/" + csid);
         });
     }
 
@@ -58,16 +58,16 @@ $(document).ready(function () {
     if ($("#eddig").exists()) {
         $("#eddig").focus();
         $("#eddig").keyup(function () {
-            var nev = $("#eddig").val();
+            const nev = $("#eddig").val();
             $.ajax({
                 beforesend: function () {
-                    $("#eddig").addClass('ui-autocomplete-loading');
+                    $("#eddig").addClass("ui-autocomplete-loading");
                 },
                 url: "getEddig",
                 complete: function () {
-                    $("#eddig").removeClass('ui-autocomplete-loading');
+                    $("#eddig").removeClass("ui-autocomplete-loading");
                 },
-                method: 'post',
+                method: "post",
                 data: "nev=" + nev
             }).done(function (bennvannak) {
                 bennvannak = JSON.parse(bennvannak);
@@ -80,11 +80,10 @@ $(document).ready(function () {
                     $("#eddigtbl").append("<td>" + bennvan.belepett + "</td>");
                     $("#eddigtbl").append("<td>" + bennvan.megjegyzes + "</td>");
                     $("#eddigtbl").append("</tr>");
-                })
+                });
             });
         });
     }
-    ;
 
     // A kezdőoldal keresőmezője
     if ($("#title").exists()) {
@@ -110,14 +109,14 @@ $(document).ready(function () {
                 return false;
             }
         }).data("ui-autocomplete")._renderItem = function (ul, item) {
-            var bel = '';
-            if (item.belepett === 'Nincs beléptetve.') {
-                bel = '<div class="d-inline bg-light text-danger ">Nincs beléptetve.</div>'
+            let bel = "";
+            if (item.belepett === "Nincs beléptetve.") {
+                bel = "<div class=\"d-inline bg-light text-danger \">Nincs beléptetve.</div>";
             } else {
-                bel = '<div class="d-inline bg-light text-primary">' + item.belepett + '</div>'
+                bel = "<div class=\"d-inline bg-light text-primary\">" + item.belepett + "</div>";
             }
-            return $('<li class="list-group-item p-0">')
-                .append("<div><strong>" + item.label + "</strong> - " + item.szul_datum + "<br>" + item.cegnev + '<br>' + bel + '</div>')
+            return $("<li class=\"list-group-item p-0\">")
+                .append("<div><strong>" + item.label + "</strong> - " + item.szul_datum + "<br>" + item.cegnev + "<br>" + bel + "</div>")
                 .appendTo(ul);
         };
     }
