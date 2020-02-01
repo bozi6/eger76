b0b24195d4c52348abe1c186efc139569b66ba3c
bozi <kontab6@gmail.com>
2020-02-02 00:46:12 +0100

    A rogton oldal magyar forditasa kesz, tobbi nyelvre kell atultetni 	new file:   app/Language/hu/Rogton.php 	modified:   app/Views/rogton/rogton.php

--- app/Views/rogton/rogton.php
+++ app/Views/rogton/rogton.php
@@ -4,25 +4,25 @@ $formdata = '';
 $labeldata = ['class' => 'control-label m-0 font-weight-bold'];
 $inputdata = ['class' => 'form-control border-0 bg-info text-warning', 'readonly' => '']; ?>
 <div class="bg-warning py-2 text-dark">
-	<div class="ml-4 text-center">Azonnali jegykiadás, a főnök kérésére.</div>
+	<div class="ml-4 text-center"><?= lang('Rogton.infoHead'); ?></div>
 	<div class="form-group col-md-4"><?= $kesz ?></div>
 	<?php echo form_open('rogton/belepes', $formdata); ?>
 	<div class="form-row ml-4">
 		<div class="form-group col-md-4">
-			<?php echo form_label('Név ha van', 'label', $labeldata); ?>
+			<?php echo form_label(lang('Rogton.infoName'), 'label', $labeldata); ?>
 			<input type="text" name="nev" class="form-control" id="name" aria-label="name"
-				   placeholder="név/Ismeretlen Szereplő">
+				   placeholder="<?= lang('Rogton.infoNamePlace'); ?>">
 		</div>
 
 		<div class="form-group col-md-4">
-			<?php echo form_label('Hány karszalag kell', 'label', $labeldata); ?>
+			<?php echo form_label(lang('Rogton.infoKarNum'), 'label', $labeldata); ?>
 			<input type="text" name="karsznum" class="form-control" id="karszalag" aria-label="karszalag"
-				   placeholder="karszalag db./0">
+				   placeholder="<?= lang('Rogton.infoKarNumPlace'); ?>">
 		</div>
 	</div>
 	<div class="form-row ml-4">
 		<div class="form-group col-md-4">
-			<?php echo form_label('Melyik fellépőhöz tartozik', 'label', $labeldata); ?>
+			<?php echo form_label(lang('Rogton.infoMelyikFellepo'), 'label', $labeldata); ?>
 			<select class="form-control" id="csoportok" name="csoportok" aria-label="csoportok">
 				<option id="1000" value="1000" selected></option>
 				<?php
